68486a774a7d14ca5706a696b8d57a0f42eef59e
andiebell <143281736+tanishkac@users.noreply.github.com>
2026-06-02 17:41:22 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(min.isEmpty()){
+            min.push(val);
+        } else {
+            min.push(Math.min(min.peek(), val));
+        }
+        
+    }
+    
+    public void pop() {
+        stack.pop();
+        min.pop();
+ 
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
